2a39552fd07cc1fdab27c72df5be1526303ae454
tony555t <tonytullah764@gmail.com>
2023-03-05 11:34:21 +0300

    homepage

---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Link } from 'react-router-dom';
+
+function Homepage(){
+    return(
+        <div>
+                   
+        </div>
+    )
+}
+export default Homepage;
